f9b49cc34e57879674903e37ef9b0ffc2549e989
AntoniaAcevedo1 <>
2022-04-25 00:47:17 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -176,7 +176,27 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 
 
 Pair * upperBound(TreeMap * tree, void* key) {
-    return NULL;
+  TreeNode * nodo;
+  char* aux= tree -> current -> pair -> key;
+  tree -> current = tree -> root;
+  nodo = NULL;
+  while (tree -> current != NULL){
+    if (tree -> lower_than(key, aux) == 1 ){
+      nodo = tree -> current;
+      if(tree -> current -> left == NULL) break;
+      tree -> current = tree -> current -> left;
+    }
+    else if (is_equal(tree, key, aux) == 1){
+      nodo = tree -> current;
+      break;
+    }
+    else{
+      if(tree -> current -> right == NULL) break;
+      tree -> current = tree -> current -> right;
+    }
+  }
+  if (nodo == NULL) return NULL;
+  return nodo -> pair;
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
